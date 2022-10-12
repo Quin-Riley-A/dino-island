1bc3332db99e2a738bfaadefa8c8b12ee4ea1927
Quin-Riley-A <quin.asselin1@gmail.com>
2022-10-11 22:37:32 -0400

    wrapped request in promise
    Co-authored-by: Peter Philavanh <efl2ice89@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
 
 function getDinos() {
-  let xhr = new XMLHttpRequest();
-  let url = 'https://dinoipsum.com/api/?format=html&words=10&paragraphs=3';
-  xhr.open('GET', url, true);
-  xhr.onload = function() {
-    if (this.status === 200) {
-      document.querySelector('#dino-list').innerHTML = this.response;
-    } else {
-      console.log('Where did all the dinosaurs go? ');
-    }
-  };
-  xhr.send();
+  let promise = new Promise(function(resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    let url = 'https://dinoipsum.com/api/?format=html&words=10&paragraphs=3';
+    xhr.onload = function() {
+      let apiResponse = JSON.parse(this.response);
+      if (this.status === 200) {
+        resolve(apiResponse);
+      } else {
+        reject(['Where did all the dinosaurs go?', this.status]);
+      }
+    };
+    xhr.open('GET', url, true);
+    xhr.send();
+  });
+
+  promise.then(function(apiResponse) {
+    displayDinos(apiResponse);
+  }, function(rejectMessage) {
+    displayError(rejectMessage);
+  });
+}
+
+//UI Logic
+
+function displayDinos(apiResponse) {
+  document.querySelector('#dino-list').innerText = apiResponse;
+}
+
+function displayError() {
+  
 }
 
 let img1 = document.querySelector('img');
